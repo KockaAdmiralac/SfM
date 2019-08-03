1608189a399081a750fcad6e8bd9e258e5bc820b
Petar Markovic <petar_ug01@yahoo.com>
2019-08-03 16:59:56 +0200

    pomozi boze

--- src/sift.cpp
+++ src/sift.cpp
@@ -1,5 +1,7 @@
 #include "sift.hpp"
-
+#include <cmath>
+using namespace std;
+using namespace cv;
 Mat gaussianBlur(Mat image, int x, double sigma)
 {
     Mat temp;
@@ -12,6 +14,47 @@ Mat downsample(Mat image)
     Mat temp;
     pyrDown(image,temp,Size(image.cols/2, image.rows/2));
     return temp;
+}
+
+Mat findKeyPoints(Mat x, Mat y, Mat z)
+{
+    Mat result = x;
+
+    for(int i=0 ; i < result.rows ; i++)
+    {   
+        for(int j=0 ; j < result.cols ; j++)
+        {
+            if((i == 0 || j == 0) || (i == result.rows-1 || j == result.cols-1))
+                result.at<double>(i,j) = 0;
+
+            else
+            {
+                double temp = y.at<double>(i,j);
+                bool flag = true;
+                for(int p = -1; p<=1;p++)
+                {
+                    for(int q = -1; q<= 1; q++)
+                    {
+                        if(
+                            (x.at<double>(i+p,j+q) > temp) ||
+                            (z.at<double>(i+p,j+q) > temp) ||
+                            (y.at<double>(i+p,j+q) > temp) 
+                            )
+                            {
+                                result.at<double>(i,j) = 0;
+                                flag = false;
+                            }
+                    }
+                }
+                if(flag)
+                    result.at<double>(i,j) =255.0;
+
+            }
+
+        }
+
+    }
+    return result;
 }
 
 
@@ -45,7 +88,7 @@ void SIFT(Mat original)
             ScaleSpace[i][j] = gaussianBlur(ScaleSpace[i][j-1],5,current_sigma);
         }
     }
-
+    // no need for normalization of the images ? 
     for(int i=0;i<octaves;i++)
     {
         for(int j =0;j<blur_levels;j++)
@@ -56,6 +99,45 @@ void SIFT(Mat original)
             str.append(".JPG");
             imwrite(str,ScaleSpace[i][j]);
         }
+    }
+    //LoG approximation:
+
+    Mat DoG[octaves-1][blur_levels-1];
+    
+    for(int i =0;i<octaves;i++)
+    {
+        for(int j =0;j<blur_levels-1;j++)
+        {
+            subtract(ScaleSpace[i][j],ScaleSpace[i][j+1],DoG[i][j]);
+            string str = "TEMP/DoGTemp/";
+            str.append(to_string(i));
+            str.append(to_string(j));
+            str.append(".JPG");
+            imwrite(str,DoG[i][j]);
+        }
+    }
+
+
+    //Finding local minima and maxima:
+
+    Mat keyPointMaps[octaves-1][blur_levels-3];
+    
+    for(int i=0; i<octaves-1; i++)
+    {
+        for(int j = 0; j<2 ; j++)
+        {
+            cout <<"iznad " << endl;
+            cout << i << " , " << j << " "<<endl;
+            keyPointMaps[i][j] = findKeyPoints(DoG[i][j],DoG[i][j+1],DoG[i][j+2]);
+            cout <<"success\n";
+            string str = "TEMP/keyPointMaps/";
+            str.append(to_string(i));
+            str.append(to_string(j));
+            str.append(".JPG");
+            imwrite(str,keyPointMaps[i][j]);
+            cout <<" end" << endl;
+        }
+        cout <<"ispod" <<endl;
     }
 
 }
